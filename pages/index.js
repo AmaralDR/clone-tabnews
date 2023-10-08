794bcdb4939a3993068e065e9064174d839cfd76
Diego Amaral <AmaralDR@users.noreply.github.com>
2023-10-08 17:57:17 +0000

    add `compose.yaml` / edit `pages/index.js`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
 function Home() {
-  return <h1>Vamos evoluir juntos?</h1>;
+  return <>
+    <h1>Vamos evoluir juntos?</h1>;
+
+    <h2>Meus repositórios - Clone tabnews</h2>
+
+    <ol>
+      <li>Principal: <a href="https://github.com/AmaralDR/clone-tabnews">Postgres</a></li>
+      <li>Fork: <a href="https://github.com/AmaralDR/clone-tabnews-mssql">SQL SERVER</a></li>
+      <li>Fork: <a href="https://github.com/AmaralDR/clone-tabnews-mongodb">MongoDB</a></li>
+    </ol>
+
+  </>
 }
 
 export default Home;
