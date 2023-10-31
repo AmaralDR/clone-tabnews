a0330438a6c4855700496fcc28b8c1b81a26941e
Diego Amaral <AmaralDR@users.noreply.github.com>
2023-10-31 00:41:30 +0000

    update `/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,17 @@
 import database from "../../../../infra/database";
 
-async function handleStatus(req, res) {
-  const result = await database.query('SELECT 1+1 as result;');
-  console.log(result.rows)
-  res.status(200).json({
-    status: 'Api Status V1'
-  })
+async function handleStatus(_req, res) {
+  const result = await database.query('SHOW max_connections; SHOW superuser_reserved_connections;');
+  const [maxConnections, superUserReservedConnections] = result;
+  const [maxConnectionsValue] = maxConnections.rows;
+  const [superuserReservedConnectionsValue] = superUserReservedConnections.rows;
+  const resultStatus = {
+    status: 'healthy',
+    updated_at: new Date(),
+    max_connections: parseInt(maxConnectionsValue["max_connections"], 10) - parseInt(superuserReservedConnectionsValue["superuser_reserved_connections"], 10),
+
+  }
+  res.status(200).json(resultStatus)
 }
 
 export default handleStatus;
