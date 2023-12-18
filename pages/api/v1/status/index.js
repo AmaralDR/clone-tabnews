471ead32c9a88f0b4e769bc67c317ad30c5567c4
Diego Amaral <AmaralDR@users.noreply.github.com>
2023-12-18 22:57:48 +0000

    finish `/api/v1/status` and closed #7

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,14 +1,28 @@
 import database from "infra/database";
 
 async function handleStatus(_req, res) {
-  const result = await database.query('SHOW max_connections; SHOW superuser_reserved_connections;');
-  const [maxConnections, superUserReservedConnections] = result;
+  const result = await database.query('SHOW server_version; SHOW max_connections; SHOW superuser_reserved_connections;');
+  const [serverVersion, maxConnections, superUserReservedConnections] = result;
+  const [serverVersionValue] = serverVersion.rows;
   const [maxConnectionsValue] = maxConnections.rows;
   const [superuserReservedConnectionsValue] = superUserReservedConnections.rows;
+
+  const resultOpened = await database.query({
+
+    text: `SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;`,
+    values: [process.env.POSTGRES_DB]
+  });
   const resultStatus = {
     status: 'healthy',
-    updated_at: new Date(),
-    max_connections: parseInt(maxConnectionsValue["max_connections"], 10) - parseInt(superuserReservedConnectionsValue["superuser_reserved_connections"], 10),
+    updated_at: new Date().toISOString(),
+    dependencies: {
+      database: {
+        version: serverVersionValue.server_version,
+        reserved_connections: parseInt(superuserReservedConnectionsValue["superuser_reserved_connections"], 10),
+        max_connections: parseInt(maxConnectionsValue["max_connections"], 10),
+        opened_connections: resultOpened.rows[0].count,
+      },
+    }
 
   }
   res.status(200).json(resultStatus)
--- test/integration/api/v1/status/get.test.js
+++ test/integration/api/v1/status/get.test.js
@@ -2,5 +2,15 @@ describe('V1 -> Api Status', () => {
   test('[GET] /api/v1/status should return 200', async () => {
     const response = await fetch('http://localhost:3000/api/v1/status');
     expect(response.status).toBe(200)
+    const responseBody = await response.json();
+    expect(responseBody).toBeDefined()
+    expect(responseBody.updated_at).toBeDefined()
+    const updatedAtParsed = new Date(responseBody.updated_at).toISOString();
+    expect(responseBody.updated_at).toBe(updatedAtParsed)
+
+    expect(responseBody.dependencies.database.version).toEqual('16.0')
+    expect(responseBody.dependencies.database.max_connections).toEqual(100)
+    expect(responseBody.dependencies.database.opened_connections).toEqual(1)
+
   })
 })
