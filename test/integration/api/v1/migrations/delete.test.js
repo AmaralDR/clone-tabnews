7d4045dc9999ccf184caf9e1c4d0bf0004455306
Diego Amaral <AmaralDR@users.noreply.github.com>
2024-04-22 01:51:35 +0000

    add `delete.test.js`

---
+++ test/integration/api/v1/migrations/delete.test.js
@@ -0,0 +1,11 @@
+
+import { cleanDatabase } from 'test/utils/database';
+beforeAll(cleanDatabase)
+describe('V1 -> Api Migrations', () => {
+  test('[DELETE] /api/v1/migrations should return 200', async () => {
+    const responseFirstRequest = await fetch('http://localhost:3000/api/v1/migrations', {
+      method: 'DELETE'
+    });
+    expect(responseFirstRequest.status).toBe(405);
+  })
+})
