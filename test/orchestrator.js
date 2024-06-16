5971a1c8293e4aedcab2411ac95bc43343b29ba0
Diego Amaral <AmaralDR@users.noreply.github.com>
2024-06-16 16:36:32 +0000

    edit `orchestrator.js` and script `pretest` #9

--- test/orchestrator.js
+++ test/orchestrator.js
@@ -5,10 +5,23 @@ async function waitForAllServices() {
   await waitForWebServer();
 
   async function waitForWebServer() {
-    return retry(fetchStatusPage);
+    return retry(fetchStatusPage, {
+      retries: 100,
+      maxTimeout: 1_000
+    });
     async function fetchStatusPage() {
-      const response = await fetch(`${getBaseUrl()}/api/v1/status`);
-      await response.json();
+      try {
+        console.time('fetchStatusPage')
+        const response = await fetch(`${getBaseUrl()}/api/v1/status`);
+        await response.json();
+
+      } catch (error) {
+        throw error;
+      } finally {
+        console.timeEnd('fetchStatusPage')
+      }
+
+
     }
   }
 }
